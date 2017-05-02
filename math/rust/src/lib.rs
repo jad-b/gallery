a70be62f297373c82c9abbe63a01ce9b5f5d78d5
jad-b <j.american.db@gmail.com>
2017-05-02 08:43:15 -0400

    Sigma summation in Rust working

--- math/rust/src/lib.rs
+++ math/rust/src/lib.rs
@@ -1,39 +1,10 @@
 #[cfg(test)]
-mod tests {
-    use numeric;
-
-    #[test]
-    fn test_fibonacci()  {
-        assert_eq!(numeric::fibonacci(0), 0);
-        assert_eq!(numeric::fibonacci(1), 1);
-        for i in 2..50 {
-            assert_eq!(numeric::fibonacci(i), numeric::fibonacci(i-1) + numeric::fibonacci(i-2));
-        }
-    }
-
-    #[test]
-    fn test_sigma() {
-        // Closure types differ when defined _outside_ of a function argument
-        // vs. on their own line, using `let`
-        assert_eq!(55, numeric::sigma(|x| x, 1, |x| x + 1, 10))
-    }
-
-    #[test]
-    fn gcd_test() {
-        assert_eq!(numeric::gcd(206, 40), 2);
-    }
-
-    #[test]
-    fn gcd_iter_test() {
-        assert_eq!(numeric::gcd_iter(206, 40), 2);
-    }
-}
-
 
 // TODO How to use any generic number for function argument?
 // RFC=https://github.com/aturon/rfcs/blob/num-reform/active/0000-num-reform.md
 // Perhaps specifying the int literal type, such as 0u32?
 pub mod numeric {
+    use std::iter::{Iterator,Sum};
 
     /* Iterataive Fibonacci number generator */
     pub fn fibonacci(n: i64) -> i64 {
@@ -50,15 +21,13 @@ pub mod numeric {
         b
     }
 
-    /* This is a stupid implementation; a better one would accept `term` and
-     * an iterator that generates our [a:b] range.
-     */
-    pub fn sigma<F,G>(term: F, a: i32, next: G, b: i32) -> i32
-        where F: Fn(i32) -> i32, G: Fn(i32) -> i32 {
-        match a > b {
-            true => 0i32,
-            false => a + sigma(term, next(a), next, b),
-        }
+    pub fn sigma<I, T, F>(iter: I, func: F) -> T
+        where I: Iterator<Item=T>,
+              T: Sum,
+              F: FnMut(T) -> T
+    {
+        // Sum over the values returned by the Iterator, applying our function at each step
+        return iter.map(func).sum()
     }
 
     pub fn gcd(a: u32, b: u32) -> u32 {
@@ -77,6 +46,36 @@ pub mod numeric {
             y = r;
         }
         x
+    }
+
+    mod tests {
+        use numeric;
+
+        #[test]
+        fn test_fibonacci()  {
+            assert_eq!(numeric::fibonacci(0), 0);
+            assert_eq!(numeric::fibonacci(1), 1);
+            for i in 2..50 {
+                assert_eq!(numeric::fibonacci(i), numeric::fibonacci(i-1) + numeric::fibonacci(i-2));
+            }
+        }
+
+        #[test]
+        fn test_sigma() {
+            // Closure types differ when defined _outside_ of a function argument
+            // vs. on their own line, using `let`
+            assert_eq!(55, numeric::sigma(1..11, |x| x))
+        }
+
+        #[test]
+        fn gcd_test() {
+            assert_eq!(numeric::gcd(206, 40), 2);
+        }
+
+        #[test]
+        fn gcd_iter_test() {
+            assert_eq!(numeric::gcd_iter(206, 40), 2);
+        }
     }
 
 }
