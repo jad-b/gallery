1a6ae0be1b1667bc8103e1ffae5d5c4338139d74
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2017-06-26 17:09:04 -0400

    Decomposing the eight queen problem

--- haskell/Haskell.md
+++ haskell/Haskell.md
@@ -3,17 +3,27 @@ You can make a Haskell library by omitting the `app/` directory and the
 executable section within the project's .cabal file. Using a stack template,
 like simple-library, could streamline this.
 
+Haskell supports __parametric__ polymorphism, where the use of _generic types_
+allow for applying the same procedures (read: procedures) to different concrete
+types.
+
+When exporting new types, you need to list which data constructors you're
+exporting as well. See BinaryMobile.hs for details. On the other hand, you can
+use `NewType(..)` to export everything associated with the type.
+
+## Monads
+
+### Level 1
+Monads provide a means of sequencing actions. This is difficult to do otherwise
+using only the functions-as-arguments style, or at least, becomes far less
+readable.
+
+### Level 2
+
+### Level 3
 IO actions can _only_ be performed  in the scope of other IO actions. The `main`
 function is the highest-level IO action allowed in Haskell. Thus, all IO
 actions, if they are to be executed, must somehow fall under `main`. This is why
 a library function that reads a file can't have the signature `FilePath ->
 String` instead of `FilePath->IO String`. Until linked under `main`, it can only
 return the action of reading a String (IO String).
-
-Haskell supports __parametric__ polymorphism, where the use of _generic types_
-allow for applying the same behaviors (read: procedures) to different concrete
-types.
-
-When exporting new types, you need to list which data constructors you're
-exporting as well. See BinaryMobile.hs for details. On the other hand, you can
-use `NewType(..)` to export everything associated with the type.
---
+++ haskell/src/EightQueens.hs
@@ -0,0 +1,54 @@
+module EightQueens where
+{- The Eight Queens problem asks what the valid placements are for eight queens
+ - on an 8 x 8 chess board.
+ -}
+
+{- This is the outline of a solution from SICP Ex. 2.42
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+-}
+{-
+queens boardSize = queenCols boardSize
+    where
+        -- Checks the queen in column k against the other queens.
+        isSafe _ pos =
+            let front = head pos
+
+        -- adjoinPosition :: Int a, Int b => a -> b -> [[(a,a)]]
+        -- Add a queen in each column of the new row
+        adjoinPosition newRow k restOfQueens = map (\qs -> (newRow, k):qs) restOfQueens
+        queenCols 0 = [[]]
+        queenCols k =
+            -- ([(a,a)] -> bool) -> [[(a,a)]] -> [[(b,b)]]
+            filter -- Filter for only valid queens
+                (\positions -> isSafe k positions)
+                -- Prepend a new queen position option to all the existing
+                -- solutions.
+                (foldl -- Given a list of (row,col) tuples, prepend one more at (newRow, k)
+                    -- [[(r_k,c_k),...,(r_1, c_1)]]
+                    (\restOfQueens -> map (\newRow -> adjoinPosition newRow k restOfQueens) [1..boardSize])
+                    queenCols (k-1))
+-}
+
+-- isSafe :: Int a => a -> [(a0,a1)] -> bool
+isSafe k queens =
+    -- "Find" the queen in column k
+    -- Compare against all other queens
+        -- If rows or columns match or are diagonal == No go
+
+-- Two points are diagonal in a 2-d space if their difference is equal in both
+-- dimensions.
+isDiagonal (a,b) (c,d) = abs (a-c) == abs(b-d)
