f2890ddf4c719cf3ef79b4b82d8e3b98c88eab27
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2018-03-16 08:20:09 -0400

    GridMap holds a Grid of PathSquares.
    This required implementing the Default + Clone traits for PathSquares, which roped in implementing Default on the Direction enum. But hey - now I have a basic understanding of Copy v. Move semantics.

--- rust/ctci/src/chapter8.rs
+++ rust/ctci/src/chapter8.rs
@@ -1,27 +1,31 @@
-mod robot_grid {
+pub struct Grid<T: Default + Clone> {
+    rows: usize,
+    cols: usize,
+    grid: Vec<Vec<T>>,
+}
 
-    pub fn main<T>(grid: &Grid<T>) -> &Vec<PathSquare>
+impl<T: Default + Clone> Grid<T> {
+    // Constructor
+    fn new(rows: usize, cols: usize) -> Self {
+        // Create an immutable 2D grid of vectors, using the default of T
+        Self {
+            rows: rows,
+            cols: cols,
+            grid: vec![vec![T::default(); cols]; rows],
+        }
+    }
+}
+
+mod robot_grid {
+    use chapter8::Grid;
+
+    pub fn main<T>(grid: &Grid<T>) -> &Vec<Direction>
         where T: Default + Clone
     {
         unimplemented!();
     }
 
-    pub struct Grid<T: Default + Clone> {
-        rows: usize,
-        cols: usize,
-        grid: Vec<Vec<T>>,
-    }
-
     impl<T: Default + Clone> Grid<T> {
-        // Constructor
-        fn new(rows: usize, cols: usize) -> Self {
-            // Create an immutable 2D grid of vectors, using the default of T
-            Self {
-                rows: rows,
-                cols: cols,
-                grid: vec![vec![T::default(); cols]; rows],
-            }
-        }
         fn right(&self, row: usize, col: usize) -> Option<T> {
             unimplemented!();
         }
@@ -34,10 +38,8 @@ mod robot_grid {
         }
     }
 
-    struct PathGrid<T> where T: Default + Clone {
-        rows: T,
-        cols: T,
-        grid: Grid<T>,
+    struct PathGrid<PathSquare: Clone + Default> {
+        grid: Grid<PathSquare>,
     }
 
     impl<T> PathGrid<T> where T: Default + Clone {
@@ -47,15 +49,22 @@ mod robot_grid {
     }
 
     // PathSquares save the calculated cost & direction at a specific square
-    pub struct PathSquare {
+    #[derive(Clone, Default)]
+    struct PathSquare {
         cost: usize,
         direction: Direction,
     }
 
     // Direction of travel from a square
-    enum Direction {
+    #[derive(Clone)]
+    pub enum Direction {
+        Nowhere,
         Right,
         Down
+    }
+
+    impl Default for Direction {
+        fn default() -> Direction { Direction::Nowhere }
     }
 
     #[cfg(test)]
