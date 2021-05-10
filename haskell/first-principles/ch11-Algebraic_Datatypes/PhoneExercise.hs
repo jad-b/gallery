d6468f5085357553a6a8433fa5627311cf75dc25
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2021-05-10 07:34:08 -0400

    Further work on PhoneExercise

--- haskell/first-principles/ch11-Algebraic_Datatypes/PhoneExercise.hs
+++ haskell/first-principles/ch11-Algebraic_Datatypes/PhoneExercise.hs
@@ -4,11 +4,45 @@ Description: "Phone exercise", pg. 450.
  -}
 module PhoneExercise where
 
+import qualified Data.Map.Strict as M
 
-data Phone = Phone
+-- validButtons = "1234567890*#"
+type Digit = Char
 
--- Convert the following conversations into the key presses
--- required to express them.
+-- Valid presses: 1 and up
+type Presses = Int
+
+data Phone = Phone (M.Map Char (Digit, Presses))
+
+mkPhone :: Phone
+mkPhone =
+  let
+    t9layout :: [(Digit, String)]
+    t9layout =
+      [ ('1', "")
+      , ('2', "abc")
+      , ('3', "def")
+      , ('4', "ghi")
+      , ('5', "jkl")
+      , ('6', "mno")
+      , ('7', "pqrs")
+      , ('8', "tuv")
+      , ('9', "wxyz")
+      -- ('*', '^')
+      -- ^Uppercases; omitted in lieu of conditional logic
+      , ('0', "_")
+      , ('#', ".,")
+      ]
+    charPresses :: M.Map Char (Digit, Presses)
+    charPresses = undefined
+    -- ^NEXT. I want to fold the t9layout into map, where each digit
+    -- can yield multiple entries.
+    -- My accum fn. will return a [(Char, (Digit, Presses)]
+    -- Example:
+    --
+    -- > f ('2', "abc") = [('a', ('2', 1)), ('b', ('2', 2)), ('c', ('2', 3))]
+
+-- |Test strings
 convo :: [String]
 convo =
   [ "Wanna play 20 questions"
@@ -22,18 +56,12 @@ convo =
   , "Just making sure rofl ur turn"
   ]
 
--- validButtons = "1234567890*#"
-type Digit = Char
-
--- Valid presses: 1 and up
-type Presses = Int
-
 -- |Convert a charcter to its digit and the number of presses required.
 -- It returns a list, as capitalized characters will include a ('*', 1) tuple.
 digitizeChar :: Phone -> Char -> [(Digit, Presses)]
 digitizeChar = undefined
 
--- |
+-- |Translates a string into an ordered list of key presses
 digitizeStr :: Phone -> String -> [(Digit, Presses)]
 digitizeStr = undefined
 
