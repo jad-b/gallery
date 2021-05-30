4de4cb3a9e828cf3ee8239da2911168b32d6f77c
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2021-05-30 05:55:55 -0400

    Ch11) Hutton's Razor

--- haskell/first-principles/ch11-Algebraic_Datatypes/HuttonsRazor.hs
+++ haskell/first-principles/ch11-Algebraic_Datatypes/HuttonsRazor.hs
@@ -5,4 +5,10 @@ data Expr
   | Add Expr Expr
 
 eval :: Expr -> Integer
-eval = error "do it to it"
+eval (Lit x) = x
+eval (Add x y) = eval x + eval y
+
+printExpr :: Expr -> String
+printExpr (Lit x) = show x
+printExpr (Add x y) = "(" ++ printExpr x ++ " + " ++ printExpr y ++ ")"
+
